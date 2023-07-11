47829d467c3f226a522a365d5e7fb3161b5f0188
Sahil Lather <sahillather002@gmail.com>
2023-07-11 17:02:08 +0530

    collision done

--- app.py
+++ app.py
@@ -82,6 +82,49 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
     
+class Pipe:
+    gap=200
+    vel=5
+
+    def __init__(self,x):
+        self.x=x
+        self.height=0
+        self.gap=100
+
+        self.top=0
+        self.bottom=0
+        self.pipe_top=pygame.transform.flip()
+        self.pipe_bottom=pipe_img
+
+        self.passed=False
+        self.set_height()
+    
+    def set_height(self):
+        self.height=random.randrange(50,450)
+        self.top=self.height-self.pipe_top.get_height()
+        self.bottom=self.height+self.gap
+
+    def move(self):
+        self.x-=self.vel
+    
+    def draw(self,win):
+        win.blit(self.pipe_top,(self.x,self.top))
+        win.blit(self.pipe_bottom,(self.x,self.bottom))
+
+    def collide(self,bird):
+        bird_mask=bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.pipe_top)
+        bottom_mask = pygame.mask.from_surface(self.pipe_bottom)
+
+        top_offset = (self.x-bird.x,self.top-round(bird.y))
+        bottom_offset=(self.x-bird.x,self.bottom-round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask,bottom_offset)
+        t_point = bird_mask.overlap(top_mask,top_offset)
+
+        if t_point or b_point:
+            return True
+        return False
 
 def draw_window(win,bird):
     win.blit(bg_img,(0,0))
